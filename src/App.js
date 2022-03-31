2def68ead3879aeaa9443e18c07cf6454ed720b9
sajidnagari <sajidnagari5@gmail.com>
2022-03-31 12:47:16 +0500

    setup project with tailwindcss

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+function App() {
+  return (
+
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      Wellcome to UExel
+    </div>
+  );
+}
+export default App;
